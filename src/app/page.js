c1e79191b26701f252b5631ec4d8e20e185fb27f
Kimiqo <michael12darko@gmail.com>
2025-03-11 14:02:42 +0000

    Migrated from typescript to javascript

---
+++ src/app/page.js
@@ -0,0 +1,22 @@
+// import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="font-[family-name:var(--font-geist-sans)]">
+      <header className="flex justify-between items-center p-2.5 bg-white h-14 w-full">
+        <div className="text-red-600">Logo</div>
+        <div className="flex text-red-600 gap-4">
+          <h4>Home</h4>
+          <h4>SOS</h4>
+          <h4>About</h4>
+          <h4>Settings</h4>
+        </div>
+      </header>
+      <br />
+      <main className="bg-amber-50 min-h-screen w-full">
+
+      </main>
+      
+    </div>
+  );
+}
