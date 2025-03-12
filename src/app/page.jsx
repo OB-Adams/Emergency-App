162cdba347e3e617a78146610557e39d5a355834
Kimiqo <michael12darko@gmail.com>
2025-03-12 15:47:34 +0000

    Added ShadCn UI, created header component

---
+++ src/app/page.jsx
@@ -0,0 +1,14 @@
+import Header from "../components/client/Header";
+
+export default function Home() {
+  return (
+    <div className="font-[family-name:var(--font-geist-sans)]">
+      <Header/>
+      <br />
+      <main className="bg-amber-50 min-h-screen w-full">
+        
+      </main>
+      
+    </div>
+  );
+}
---
+++ src/components/client/Header.jsx
@@ -0,0 +1,18 @@
+import { Button } from "../ui/button";
+
+
+export default function Header() {
+    return (
+      <header className="flex justify-between items-center p-2.5 m-3.5 bg-white h-14 rounded-2xl drop-shadow-lg shadow-red-600">
+        <div className="text-red-600 font-bold">Emergency Response App</div>
+        <nav className="flex text-red-600 gap-4">
+          <a href="/signup" className="hover:underline">Sign Up</a>
+          <a href="/login" className="hover:underline">Login</a>
+          <a href="/homepage" className="hover:underline">Home</a>
+          <Button
+          className={"w-2 h-6.5 bg-red-600"}
+          />
+        </nav>
+      </header>
+    );
+  }
