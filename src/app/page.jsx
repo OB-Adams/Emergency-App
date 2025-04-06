6f345d0703b114325148641b3d8301e42beb1e05
Delphineaopratt <delphineaopratt@gmail.com>
2025-04-06 16:13:25 +0000

    Dashboard created

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,12 +1,13 @@
 import Header from "../components/client/Header";
 import SOSpage from "../components/client/SOSpage";
+import ResponderDashboard from "./responder_dashboard/page";
 
 export default function Home() {
   return (
     <div className="font-[family-name:var(--font-geist-sans)]">
       <Header/>
       <main className="bg-white items-center min-h-screen rounded-2xl border border-gray-200 mx-3.5">
-        <SOSpage/>
+       <ResponderDashboard/>
       </main>
     </div>
   );
